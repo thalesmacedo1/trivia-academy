c66313b35be280e3418ebdd5bfe0ec48f9cba9de
Thales Macêdo <thalesmacedo1@live.com>
2021-02-03 23:57:04 -0300

    Radio button config

--- lib/trivia_screen.dart
+++ lib/trivia_screen.dart
@@ -6,6 +6,8 @@ class TriviaScreen extends StatefulWidget {
 }
 
 class _TriviaScreenState extends State<TriviaScreen> {
+  int answer = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +16,82 @@ class _TriviaScreenState extends State<TriviaScreen> {
         automaticallyImplyLeading: false,
         backgroundColor: Color(0xff374952),
         title: Image(image: AssetImage('lib/assets/icon.png')),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(32, 56, 32, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Question 1',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text('What is Flutter?'),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.15),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(1, 3),
+                      )
+                    ],
+                  ),
+                  child: RadioListTile(
+                    value: 1,
+                    groupValue: answer,
+                    title: Text('É um framework da Google'),
+                    onChanged: (int value) => {
+                      setState(() {
+                        answer = value;
+                      })
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.15),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(1, 3),
+                      )
+                    ],
+                  ),
+                  child: RadioListTile(
+                    value: 2,
+                    groupValue: answer,
+                    title: Text('É um novo sistema operacional'),
+                    onChanged: (int value) => {
+                      setState(() {
+                        answer = value;
+                      })
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
